13a5f7075cd1d09d444095a65cdb58d8cc9837a9
Harsha Cherukuri <hcheruku@redhat.com>
2026-03-24 10:49:10 -0400

    Fix ansible-doc errors

--- plugins/modules/product_download.py
+++ plugins/modules/product_download.py
@@ -44,9 +44,58 @@ short_description: Downloads products from the JBoss Network API.
 description:
     - Downloads products from the JBoss Network API.
 extends_documentation_fragment:
-    - files
-    - middleware_automation.common.jbossnetwork_connection_options
+    - ansible.builtin.files
 options:
+    api_url:
+        description:
+            - Address of the JBoss Network API.
+        type: str
+        required: false
+        default: 'https://jbossnetwork.api.redhat.com'
+    client_id:
+        description:
+            - Client ID associated with to download a product from the JBoss Network API.
+            - If value not set, will try environment variable C(REDHAT_PRODUCT_DOWNLOAD_CLIENT_ID)
+        type: str
+        required: false
+    client_secret:
+        description:
+            - Client Secret associated with to download a product from the JBoss Network API.
+            - If value not set, will try environment variable C(REDHAT_PRODUCT_DOWNLOAD_CLIENT_SECRET)
+        type: str
+        required: false
+    sso_url:
+        description:
+            - Address of the Red Hat SSO Server.
+        type: str
+        required: false
+        default: 'https://sso.redhat.com'
+    validate_certs:
+        description:
+        - If C(false), SSL certificates will not be validated.
+        type: bool
+        default: yes
+        required: false
+    product_category:
+        description:
+            - Type of the Product Category
+        type: str
+        required: false
+    product_id:
+        description:
+        - Product Id for the Redhat customer portal
+        type: str
+        required: false
+    product_type:
+        description:
+            - Type of the Product
+        type: str
+        required: false
+    product_version:
+        description:
+        - Product Version to be downloaded.
+        type: str
+        required: false
     dest:
         description:
             - Absolute  .
